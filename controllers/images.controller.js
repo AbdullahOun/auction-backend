dd9f1b0ceca3e7fee1d817815dfa3461034cdf93
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-05-07 00:52:04 +0300

    feat: images upload and delete endpoints

--- controllers/images.controller.js
+++ controllers/images.controller.js
@@ -1,23 +1,38 @@
 const asyncWrapper = require('../middlewares/asyncWrapper')
-const httpStatusText = require('../utils/httpStatusText')
-const appError = require('../utils/appError')
+const AppError = require('../utils/appError')
+const AppResponse = require('../utils/appResponse')
+const { MODEL_MESSAGES, HTTP_STATUS_CODES } = require('../utils/constants')
+const deleteImage = require('../utils/deleteImages')
+/**
+ * Middleware to handle image uploads.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 const uploadImage = asyncWrapper(async (req, res, next) => {
+    // Check if a file was uploaded
     if (!req.file) {
-        const error = appError.create(
-            'You must add a file',
-            400,
-            httpStatusText.FAIL
+        // If no file was uploaded, return an error response
+        next(
+            new AppError(
+                MODEL_MESSAGES.file.missing,
+                HTTP_STATUS_CODES.BAD_REQUEST
+            )
         )
-        next(error)
         return
     }
-    res.status(201).json({
-        status: httpStatusText.SUCCESS,
-        data: { imageUrl: req.file.filename },
-        error: null,
-    })
+    // If a file was uploaded successfully, return the image URL in the response
+    res.status(HTTP_STATUS_CODES.CREATED).json(
+        new AppResponse({ imageUrl: req.file.filename })
+    )
+})
+const removeImage = asyncWrapper(async (req, res, next) => {
+    const imagePath = req.params.imagePath
+    await deleteImage([imagePath])
+    res.status(HTTP_STATUS_CODES.CREATED).json(new AppResponse(null))
 })
 
 module.exports = {
     uploadImage,
+    removeImage,
 }
