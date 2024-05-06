59e1bc357ffce1ef7d14821834f80fa04264ad70
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-05-07 00:48:50 +0300

    feat: database level validation and indexing

--- models/message.model.js
+++ models/message.model.js
@@ -1,19 +1,58 @@
 const mongoose = require('mongoose')
+
+/**
+ * Schema for the Message model.
+ */
 const messageSchema = new mongoose.Schema(
     {
-        chatRoomId: {
+        /**
+         * The chat room associated with the message.
+         * @type {mongoose.Schema.Types.ObjectId}
+         * @required
+         * @ref ChatRoom
+         * @validate Custom validator to check if chatRoom exists in the ChatRoom collection
+         */
+        chatRoom: {
             type: mongoose.Schema.Types.ObjectId,
             required: true,
             ref: 'ChatRoom',
+            validate: {
+                validator: async function (value) {
+                    const chatRoom = await mongoose
+                        .model('ChatRoom')
+                        .findById(value)
+                    return chatRoom !== null
+                },
+                message: 'Chat room does not exist',
+            },
         },
+        /**
+         * The content of the message.
+         * @type {String}
+         * @required
+         */
         content: {
             type: String,
             required: true,
         },
-        senderId: {
+        /**
+         * The sender of the message.
+         * @type {mongoose.Schema.Types.ObjectId}
+         * @required
+         * @ref User
+         * @validate Custom validator to check if sender exists in the User collection
+         */
+        sender: {
             type: mongoose.Schema.Types.ObjectId,
             required: true,
             ref: 'User',
+            validate: {
+                validator: async function (value) {
+                    const user = await mongoose.model('User').findById(value)
+                    return user !== null
+                },
+                message: 'Sender does not exist',
+            },
         },
     },
     { timestamps: true }
