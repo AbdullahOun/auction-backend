e8d7db3c1006ca0f16345c0adbb426e59d8befca
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-05-07 00:47:25 +0300

    fix: strong equality algo is used

--- utils/encryption.js
+++ utils/encryption.js
@@ -1,18 +1,41 @@
-
 const crypto = require('node:crypto')
 
-const KEYLEN = +process.env.KEYLEN;
-const SALT = process.env.SALT;
+/**
+ * The length of the key used for hashing passwords.
+ * @type {number}
+ */
+const KEYLEN = +process.env.KEYLEN
+
+/**
+ * The salt used for hashing passwords.
+ * @type {string}
+ */
+const SALT = process.env.SALT
+
+/**
+ * Hashes a password using scrypt algorithm.
+ * @param {string} password - The password to be hashed.
+ * @returns {string} The hashed password.
+ */
 function hashPassword(password) {
-    return  crypto.scryptSync(password,  SALT,KEYLEN).toString('hex');
+    return crypto.scryptSync(password, SALT, KEYLEN).toString('hex')
 }
 
- function verifyPassword(password , userPassword) {
-    hashedPassword =  hashPassword(password);
-    return hashedPassword == userPassword
+/**
+ * Verifies a password by comparing it with a hashed password.
+ * @param {string} password - The password to be verified.
+ * @param {string} userPassword - The hashed password to compare against.
+ * @returns {boolean} True if the password matches the hashed password, otherwise false.
+ */
+function verifyPassword(password, userPassword) {
+    const hashedPassword = hashPassword(password)
+    return crypto.timingSafeEqual(
+        Buffer.from(hashedPassword),
+        Buffer.from(userPassword)
+    )
 }
 
 module.exports = {
     hashPassword,
-    verifyPassword
+    verifyPassword,
 }
