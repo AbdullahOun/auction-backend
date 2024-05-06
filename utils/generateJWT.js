6e9a3d44ace617108a102442d2247fc9189fa061
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-05-07 00:46:24 +0300

    refactor: add jsDocs

--- utils/generateJWT.js
+++ utils/generateJWT.js
@@ -1,5 +1,11 @@
 const jwt = require('jsonwebtoken')
-module.exports = async (payload) => {
+
+/**
+ * Generates a JWT token with the provided payload.
+ * @param {object} payload - The payload to be included in the token.
+ * @returns {Promise<string>} A Promise that resolves with the generated JWT token.
+ */
+module.exports = async function generateToken(payload) {
     const token = await jwt.sign(payload, process.env.JWT_SECRET_KEY, {
         expiresIn: '30d',
     })
