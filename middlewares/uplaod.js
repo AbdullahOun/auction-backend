ec5659e27bd86a2ce954c02b8b7befb2fa2f4b1a
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-06-30 07:17:19 +0300

    fix: formmating issues

--- middlewares/uplaod.js
+++ middlewares/uplaod.js
@@ -1,30 +1,26 @@
 const multer = require('multer')
 const AppError = require('../utils/appError')
-const { MODEL_MESSAGES, HTTP_STATUS_CODES } = require('../utils/constants')
+const { HTTP_STATUS_CODES } = require('../utils/constants')
 
 /**
- * @description File filter function for multer to filter image files.
- * @param {Object} req - The request object.
- * @param {Object} file - The file object.
- * @param {Function} cb - The callback function.
+ * Function to filter uploaded files based on their mimetype.
+ * Allows only 'image/*' types.
+ * @param {object} req - Express request object.
+ * @param {object} file - Uploaded file object.
+ * @param {Function} cb - Callback function (cb(error, boolean)).
  */
 const fileFilter = (req, file, cb) => {
     const imageType = file.mimetype.split('/')[0]
     if (imageType === 'image') {
         return cb(null, true)
     } else {
-        return cb(
-            new AppError(
-                MODEL_MESSAGES.file.onlyImages,
-                HTTP_STATUS_CODES.BAD_REQUEST
-            ),
-            false
-        )
+        return cb(new AppError('Only images are allowed', HTTP_STATUS_CODES.BAD_REQUEST), false)
     }
 }
 
 /**
- * @description Multer upload configuration.
+ * Multer configuration for handling file uploads.
+ * Uses memory storage and the defined file filter.
  */
 const upload = multer({
     storage: multer.memoryStorage(),
