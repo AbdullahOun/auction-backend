f9174f2a9081dd3c3ef37952a523517ca8c38e9f
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-06-30 07:37:45 +0300

    fix: remove unnessesary docs

--- utils/appResponse.js
+++ utils/appResponse.js
@@ -1,28 +1,7 @@
-/**
- * Represents a response from the application.
- */
 class AppResponse {
-    /**
-     * Create an instance of AppResponse.
-     * @param {any} data - The data to be included in the response.
-     */
     constructor(data) {
-        /**
-         * Error information. If null, the request was successful.
-         * @type {Object | null}
-         */
         this.error = null
-
-        /**
-         * The status text of the response.
-         * @type {string}
-         */
         this.statusText = 'Success'
-
-        /**
-         * The data included in the response.
-         * @type {any}
-         */
         this.data = data
     }
 }
