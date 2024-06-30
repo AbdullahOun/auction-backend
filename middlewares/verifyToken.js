bd506161094a041c668ef568aa74443c1709dcfd
abdullah-muhammedd <dev.abdullah.muhammed@gmail.com>
2024-06-30 07:18:13 +0300

    fix: formmating issues

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,27 +1,25 @@
 const jwt = require('jsonwebtoken')
 const AppError = require('../utils/appError')
-const { HTTP_STATUS_CODES, MODEL_MESSAGES } = require('../utils/constants')
+const { HTTP_STATUS_CODES } = require('../utils/constants')
 const process = require('process')
+const { logger } = require('../utils/logging/logger')
+
 require('dotenv').config()
 
 /**
- * @description Middleware to verify JWT token from request headers.
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- * @param {Function} next - The next middleware function.
- * @returns {Function} - The next middleware function or an error.
+ * Middleware function to verify JWT token from Authorization header.
+ * Sets decoded token in req.decodedToken if valid.
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {Function} next - Express next function.
+ * @returns {void}
+ * @throws {AppError} - Throws unauthorized error if token is missing or invalid.
  */
 const verifyToken = (req, res, next) => {
-    const authHeader =
-        req.headers['Authorization'] || req.headers['authorization']
+    const authHeader = req.headers['Authorization'] || req.headers['authorization']
 
     if (!authHeader) {
-        return next(
-            new AppError(
-                MODEL_MESSAGES.user.tokenRequired,
-                HTTP_STATUS_CODES.UNAUTHORIZED
-            )
-        )
+        return next(new AppError('Token is required', HTTP_STATUS_CODES.UNAUTHORIZED))
     }
     const token = authHeader.split(' ')[1]
 
@@ -30,12 +28,8 @@ const verifyToken = (req, res, next) => {
         req.decodedToken = decodedToken
         return next()
     } catch (err) {
-        return next(
-            new AppError(
-                MODEL_MESSAGES.user.invalidToken,
-                HTTP_STATUS_CODES.UNAUTHORIZED
-            )
-        )
+        logger.error(err.message)
+        return next(new AppError('Invalid token', HTTP_STATUS_CODES.UNAUTHORIZED))
     }
 }
 
